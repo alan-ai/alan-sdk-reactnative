431ff5acdeacc1a7e619cbde879a4aecb084faa4
Aleksey Makarkin <aleksey.makarkin@gmail.com>
2020-10-13 18:07:59 +0300

    rm testapp

--- testapp/node_modules/react-native-safe-area-context/lib/typescript/example/src/components/ReactNavigationSettingsScreen.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-declare const _default: (props: import("@react-navigation/stack").StackScreenProps<import("../types/Navigation").Routes, "Settings">) => JSX.Element;
-export default _default;
